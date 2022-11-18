d4445548efd4f46851c75a3899b18c930144fc2b
Jason Cheng <cheng.ky@csit.gov.sg>
2022-11-18 15:56:11 +0800

    Only frontend-stuff, added tailwind config

--- backend/app/static/documentation.md
+++
@@ -1,3 +0,0 @@
-# API docs
-
-This is where documentation on the API endpoints can be found.
--- frontend/src/Application.tsx
+++ frontend/src/Application.tsx
@@ -1,69 +1,12 @@
 import './styles.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import * as React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { LoadingSpinner } from './components/LoadingSpinner';
-import RequireAuth from './components/RequireAuth';
-import { AboutPage } from './pages/About';
-import LandingPage from './pages/Landing';
-import { Logout } from './pages/Logout';
-import { NumberPage } from './pages/Number';
-import { Todo } from './pages/Todo';
+import React from 'react';
 
 export interface IApplicationProps {}
 
 export function Application(props: IApplicationProps) {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<LandingPage defaultActiveKey="login" />} />
-                <Route path="/register" element={<LandingPage defaultActiveKey="register" />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/loading" element={<LoadingSpinner />} />
-                <Route
-                    path="/"
-                    element={
-                        <RequireAuth>
-                            <Navigate to="/todo" />
-                        </RequireAuth>
-                    }
-                />
-                <Route
-                    path="about"
-                    element={
-                        <RequireAuth>
-                            <AboutPage />
-                        </RequireAuth>
-                    }
-                />
-                <Route
-                    path="todo"
-                    element={
-                        <RequireAuth>
-                            <Todo />
-                        </RequireAuth>
-                    }
-                />
-                <Route path="number">
-                    <Route
-                        index
-                        element={
-                            <RequireAuth>
-                                <NumberPage />
-                            </RequireAuth>
-                        }
-                    />
-                    <Route
-                        path=":number"
-                        element={
-                            <RequireAuth>
-                                <NumberPage />
-                            </RequireAuth>
-                        }
-                    />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <div>
+            <h1>Hello World!</h1>
+        </div>
     );
 }
