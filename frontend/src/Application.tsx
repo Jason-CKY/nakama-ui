2daa4f0fbb744c8b6c1de26bf22d2faf49dbf5d6
Jason Cheng <cheng.ky@csit.gov.sg>
2022-11-21 11:48:50 +0800

    Added headers and theme toggles

--- frontend/src/Application.tsx
+++ frontend/src/Application.tsx
@@ -1,12 +1,24 @@
 import './styles.css';
-import React from 'react';
+
+import React, { useState } from 'react';
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+
+import { HeaderComponent } from './components/Header';
+import { Login } from './components/Login';
 
 export interface IApplicationProps {}
 
 export function Application(props: IApplicationProps) {
+    const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+    const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+                <div className="flex flex-col">
+                    <HeaderComponent />
+                    <Login />
+                </div>
+            </MantineProvider>
+        </ColorSchemeProvider>
     );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,40 @@
-import * as React from 'react';
+import React from 'react';
 
-export interface IHeaderProps {}
+import { createStyles, Menu, Center, Header, Container, Group, Button, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconChevronDown } from '@tabler/icons';
+import { MantineLogo } from '@mantine/ds';
+import Logo from '../assets/logo.jpg';
+import { ThemeToggle } from './ThemeToggle';
 
-export function Header(props: IHeaderProps) {
-    return <div></div>;
+const HEADER_HEIGHT = 80;
+
+const useStyles = createStyles((theme) => ({
+    header: {
+        paddingTop: theme.spacing.sm,
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        borderBottom: `1px solid ${theme.colorScheme === 'dark' ? 'transparent' : theme.colors.gray[2]}`,
+        marginBottom: 120,
+        display: 'flex'
+    },
+    inner: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+}));
+
+interface IHeaderProps {}
+
+export function HeaderComponent({}: IHeaderProps) {
+    const { classes } = useStyles();
+
+    return (
+        <Header height={HEADER_HEIGHT} className={classes.header} mb={120}>
+            <div className="flex justify-between w-full">
+                <img src={Logo} className="rounded-full mx-5 mb-2 " />
+                <ThemeToggle className="mx-5" />
+            </div>
+        </Header>
+    );
 }
